8bfb18ab466fbe476b144e32d129ca967fee6262
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-09-11 23:21:13 -0500

    Adding sanbox test

--- tests/agent-tests.js
+++ tests/agent-tests.js
@@ -2,20 +2,24 @@
 
 const test = require('ava')
 const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+
 const config = {
   loggin: function () {}
 }
 
 const MetricStub = {
-  belongsTo: function () {}
+  belongsTo: sinon.spy()
 }
 
 let AgentSub = null
 let db = null
+let sandbox = null
 
 test.beforeEach(async () => {
+  sandbox = sinon.createSandbox()
   AgentSub = {
-    hasMany: function () {}
+    hasMany: sandbox.spy()
   }
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentSub,
@@ -24,6 +28,17 @@ test.beforeEach(async () => {
   db = await setupDatabase(config)
 })
 
+test.afterEach(() => {
+  sandbox && sandbox.restore()
+})
+
 test('Agent', t => {
   t.truthy(db.Agent, 'Agent service should exist')
+})
+
+test.serial('Setup', t => {
+  t.true(AgentSub.hasMany.called, 'AgentModel.hasMany was executed')
+  t.true(AgentSub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
+  t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
+  t.true(MetricStub.belongsTo.calledWith(AgentSub), 'Argument needs to be the AgentModel')
 })
