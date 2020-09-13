5c2641a867e8beba39a742cec4bb7b0479e88a72
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-13 11:59:01 -0500

    Adding the agent connection

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -7,6 +7,7 @@ const chalk = require('chalk')
 const db = require('platziverse-app')
 const conf = require('../platziverse-db/config')
 const config = conf()
+const { parsePayload } = require('./utils')
 
 const backend = {
   type: 'redis',
@@ -19,30 +20,59 @@ const settings = {
   backend
 }
 
-/* const config = {
-  database: process.env.DB_NAME || 'platziverse',
-  username: process.env.DB_USER || 'platzi',
-  password: process.env.DB_PASS || 'platzi',
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-  logging: s => debug(s),
-} */
-
 const server = new mosca.Server(settings)
-
+const clients = new Map()
 let Agent, Metric
 
 server.on('clientConnected', client => {
   debug(`Client Connected: ${client.id}`)
+  clients.set(client.id, null)
 })
 
 server.on('clientDisconnected', client => {
   debug(`Client Disconnected: ${client.id}`)
 })
 
-server.on('published', (packet, client) => {
+server.on('published', async (packet, client) => {
   debug(`Recieved: ${packet.topic}`)
-  debug(`Payload: ${packet.payload}`)
+  let payload = null
+  switch (packet.topic) {
+    case 'agent/connected':
+    case 'agent/disconnected':
+      debug(`Payload: ${packet.payload}`)
+      break
+    case 'agent/message':
+      debug(`Payload: ${packet.payload}`)
+      payload = parsePayload(packet.payload)
+      if (payload) {
+        payload.agent.connected = true
+        let agent
+        try {
+          agent = await Agent.createOrUpdate(payload.agent)
+        } catch (e) {
+          return handleError(e)
+        }
+        debug(`Agent ${agent.uuid} saved`)
+
+        // Notify Agent is connected
+        if (!clients.get(client.id)) {
+          clients.set(client.id, agent)
+          server.publish({
+            topic: 'agent/connected',
+            payload: JSON.stringify({
+              agent: {
+                uuid: agent.uuid,
+                name: agent.name,
+                hostname: agent.hostname,
+                pid: agent.pid,
+                connected: agent.connected
+              }
+            })
+          })
+        }
+      }
+      break
+  }
 })
 
 server.on('ready', async () => {
@@ -60,6 +90,11 @@ function handleFatalError (err) {
   console.error(`${chalk.bgRed.black('[Fatal error]:')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
+}
+
+function handleError (err) {
+  console.error(`${chalk.bgRed.black('[Fatal error]:')} ${err.message}`)
+  console.error(err.stack)
 }
 
 process.on('uncaughtException', handleFatalError)
