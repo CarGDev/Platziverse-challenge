802106e8d1901343144be13bf12814d6413bc6b1
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-13 10:35:12 -0500

    creating the server

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -1,2 +1,36 @@
 'use strict'
 
+const debug = require('debug')('platziverse:mqtt')
+const mosca = require('mosca')
+const redis = require('redis')
+const chalk = require('chalk')
+
+const backend = {
+  type: 'redis',
+  redis,
+  return_buffers: true
+}
+
+const settings = {
+  port: 1883,
+  backend
+}
+
+const server = new mosca.Server(settings)
+
+server.on('clientConnected', client => {
+  debug(`Client Connected: ${client.id}`)
+})
+
+server.on('clientDisconnected', client => {
+  debug(`Client Disconnected: ${client.id}`)
+})
+
+server.on('published', (packet, client) => {
+  debug(`Recieved: ${packet.topic}`)
+  debug(`Payload: ${packet.payload}`)
+})
+
+server.on('ready', () => {
+  console.log(`${chalk.bgGreen.black('[platziverse-mqtt]:')} server is running`)
+})
