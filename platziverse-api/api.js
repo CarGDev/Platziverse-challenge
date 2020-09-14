53e0487e350ed11e3004a30ed7113622ff06e21a
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-13 21:58:05 -0500

    Routes created

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -5,4 +5,16 @@ const debug = require('debug')('platziverse:api:routes')
 const express = require('express')
 const api = express.Router()
 
+api.get('/agents', (req, res) => {})
+api.get('/agents/:uuid', (req, res) => {
+  const { uuid } = req.params
+})
+api.get('metrics/:uuid', (req, res) => {
+  const { uuid } = req.params
+})
+api.get('/metrics/:uuid/:type', (req, res) => {
+  const { uuid, type } = req.params
+})
+
+
 module.exports = api
