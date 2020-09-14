02fd2e2051c7ce0ae6bfe5444f473fa5f9aad8e8
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-14 14:16:35 -0500

    Creating test for api routes

--- platziverse-api/api.js
+++ platziverse-api/api.js
@@ -1,16 +1,40 @@
 'use strict'
 
 const debug = require('debug')('platziverse:api:routes')
-
 const express = require('express')
-const api = express.Router()
+const db = require("platziverse-app")
+const asyncify = require('express-asyncify')
+const config = require('../platziverse-db/config')
+
+const api = asyncify(express.Router())
+
+let services
+let Agent
+let Metric
+
+api.use('*', async (req, res, next) => {
+  if (!services) {
+    try {
+      services = await db(config())
+    } catch (e) {
+      return next(e)
+    }
+    Agent = services.Agent
+    Metric = services.Metric
+  }
+  next()
+})
 
 api.get('/agents', (req, res) => {
   debug('A request has come to /agents')
   res.send({})
 })
-api.get('/agents/:uuid', (req, res) => {
+api.get('/agents/:uuid', (req, res, next) => {
   const { uuid } = req.params
+
+  if (uuid !== 'yyy') {
+    return next(new Error('Agent not found'))
+  }
   res.send({ uuid })
 })
 api.get('metrics/:uuid', (req, res) => {
